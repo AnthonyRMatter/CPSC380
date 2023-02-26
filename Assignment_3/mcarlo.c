eff72f28f26ed0e6e605177fc85bdbfd3fb34fc6
AnthonyMatter <anthonymatter12@gmail.com>
2023-02-25 17:04:38 -0800

    Added assignments to file

--- Assignment_3/mcarlo.c
+++ Assignment_3/mcarlo.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <math.h>
+#include <sys/wait.h>
 
 /* THE PORTION BELOW IS JUST A CODE SEGMENT RIGHT NOW, IT WILL CHANGE LATER*/
 
@@ -9,11 +13,15 @@ double random_double()
     return random() / ((double)RAND_MAX + 1);
 }
 
-int main (void)
+int main(int argc, char* argv[])
 {
-    int x, y, npoints, hit_count;
+    int hit_count = 0;
+    int npoints = atoi(argv[1]);
+    printf("%d\n", npoints);
     /*Check for points inside circle*/
-    for (int i = 0; i < npoints; ++i)
+    int i;
+    double x, y;
+    for (i = 0; i < npoints; ++i)
     {
         /* Generates random numbers between -1.0 and +1.0 (exclusive) */
         x = random_double() * 2.0 - 1.0;
@@ -24,4 +32,9 @@ int main (void)
             ++hit_count;
         }
     }
+
+    printf("%d\n", hit_count);
+    double pi = 4 * ((double)hit_count/npoints);
+    printf("%f\n", pi);
+    return 0;
 }
