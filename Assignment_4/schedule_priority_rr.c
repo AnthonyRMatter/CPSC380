840acbd2fa7ad8e175a6902806396ff93b8bdac0
AnthonyMatter <anthonymatter12@gmail.com>
2023-03-08 14:11:52 -0800

    Added a priority round-robin scheduler and adjusted round robin

--- Assignment_4/schedule_priority_rr.c
+++ Assignment_4/schedule_priority_rr.c
@@ -3,13 +3,13 @@
 #include "schedulers.h"
 #include "list.h"
 
-void RR()
+void PriorityRR()
 {
     // Take in tasks into an array
 
     // Call traverse list function in list.c to check the current order of tasks
 
-    // Run a sorting algorithm to sort the array from lowest to highest based on time quantum
+    // Run a sorting algorithm to sort the array from lowest to highest based on time quantum and priority
 
     // Traverse through list and execute tasks starting at index 0 through end of list
     
