4c828aba6563504efdb776e59539c65f9e3cd71e
AnthonyMatter <anthonymatter12@gmail.com>
2023-03-12 00:11:21 -0800

    Added a template for Round Robin

--- Assignment_4/schedule_rr.c
+++ Assignment_4/schedule_rr.c
@@ -1,16 +1,32 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "schedulers.h"
 #include "list.h"
+#include "cpu.h"
 
-void RR()
+struct node** head;
+
+void add(char *name, int priority, int burst)
 {
-    // Take in tasks into an array
+    Task* newTask = malloc(sizeof(Task));
 
-    // Call traverse list function in list.c to check the current order of tasks
+    newTask->name = name;
+    newTask->priority = priority;
+    newTask->burst = burst;
 
-    // Run a sorting algorithm to sort the array from lowest to highest based on time quantum
+    insertBack(head, newTask);
 
-    // Traverse through list and execute tasks starting at index 0 through end of list
+}
+
+void schedule()
+{
+    struct node* sort = *head;
+    
+    struct node* temp = *head;
+    while(temp != NULL){
+        run(temp->task,temp->task->burst);
+        temp = temp->next;
+    }
     
 }
