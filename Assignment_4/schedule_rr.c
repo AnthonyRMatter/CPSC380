08224f4b1ad3cd7f34fa14b3eeedd65ec4855b1d
AnthonyMatter <anthonymatter12@gmail.com>
2023-03-16 18:21:12 -0700

    Cleaned up the comments

--- Assignment_4/schedule_rr.c
+++ Assignment_4/schedule_rr.c
@@ -24,26 +24,14 @@ void schedule()
     int burstTime = 0;
 
     /*Traverse through the list*/
-    while(sort != NULL) // LOOK OUT FOR: Might have to change this to a different node, since the length of this node might alter within the loop
+    while(sort != NULL)
     {
         /*If the burst is bigger than time quantum, set burst to remainder and put remainder at end of list*/
-        //printf("Check Contents of Burst: %d\n", sort->task->burst);
         burstTime = sort->task->burst;
+
         if(burstTime - QUANTUM > 0)
         {
-            //printf("Test\n");
-            /*Create new node with all contents of node that has bigger burst than Time Quantum*/
-            //printf("Test\n");
-            // newList->task->priority = sort->task->priority;
-            // newList->task->name = sort->task->name;
-            // newList->task->burst = sort->task->burst - QUANTUM;
             add(sort->task->name, sort->task->priority, sort->task->burst - QUANTUM);
-            // printf("Contents of Kicked-Out Node:\n");
-            // printf("Name: %s\n", newList->task->name);
-            // printf("Burst: %d\n", newList->task->burst);
-            // printf("Priority: %d\n", newList->task->priority);
-            // tail->next = newList; // Set new end of list
-            // printf("Task burst: %d\n", tail->next->task->burst);
 
             sort->task->burst = QUANTUM;
         }
