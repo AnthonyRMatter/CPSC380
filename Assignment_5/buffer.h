186dc779a246ddfff2ef2128579b31aca0c6db80
AnthonyMatter <anthonymatter12@gmail.com>
2023-04-04 15:07:26 -0700

    Added a checksum algorithm as well as set up material in main function for command line input, create producer/consumer threads. However, these are untested.

--- Assignment_5/buffer.h
+++ Assignment_5/buffer.h
@@ -1,9 +1,10 @@
 #include <stdint.h> 
 #ifndef BUFFER.H
 #define BUFFER_SIZE 30
+#define NUM_ITEMS 15
 typedef struct{
     uint16_t cksum;
     uint8_t buffer[BUFFER_SIZE];
 } buffer_item; 
 #endif
-//struct buffer_item[NUM_ITEMS]; 
+struct buffer_item item[NUM_ITEMS]; 
--- Assignment_5/prodcon.c
+++ Assignment_5/prodcon.c
@@ -16,20 +16,78 @@
 #include "buffer.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include<unistd.h>
+#include <unistd.h>
+#include <pthread.h>
 
-buffer_item buffer[BUFFER_SIZE]; 
+
+buffer_item buffer[BUFFER_SIZE];
+void *Producer(void *param); /*Producer Thread*/
+void *Consumer(void *param); /*Consumer Thread*/
+int thread;
+
+unsigned int checksum(unsigned int *ptr, int numberbytes)
+{
+    unsigned long total;
+    for(total = 0; numberbytes > 1; numberbytes -= 2)
+    {
+        total += *ptr++;
+    }
+    if(numberbytes == 1)
+    {
+        total += *(unsigned char*)ptr;
+    }
+    
+    total = (total >> 16) + (total & 0xffff);
+    total += (total >> 16);
+    return (unsigned int)(total);
+}
+
+
 
 int insert_item(buffer_item *item){
+    // Calculate CheckSum
+    int numberbytes = strlen(item->buffer);
+    unsigned int *ptr = (unsigned int *)item->buffer;
+    item->cksum = checksum(ptr, numberbytes);
+    
     //return 0 if successful, -1 if error
 }
 
 int remove_item(buffer_item *item){
     //return 0 if successful, -1 if error
 }
+
 int main(int argc, char *argv[]){
     //command line arguments 
-    //initialize buffer 
+    int producerThreads = atoi(argv[2]);
+    int consumerThreads = atoi(argv[3]);
+    //initialize buffer
+    buffer_item *item = malloc(sizeof(buffer_item));
+
+    //Create Producer and Consumer Thread IDs and Attributes
+    pthread_t producerID;
+    pthread_t consumerID;
+    pthread_attr_t producerAttr;
+    pthread_attr_t consumerAttr;
+
+    // Create Producer Threads
+    while(producerThreads > 0)
+    {
+        pthread_attr_init(&producerAttr);
+        thread = pthread_create(&producerID, &producerAttr, Producer, NULL);
+        producerThreads--;
+    }
+
+    // Create Consumer Threads
+    while(consumerThreads > 0)
+    {
+        pthread_attr_init(&consumerAttr);
+        thread = pthread_create(&consumerID, &consumerAttr, Consumer, NULL);
+        consumerThreads--;
+    }
+
+
+
     //producer thread calls insert()
     //consumer thread calls remove()
     
@@ -37,4 +95,15 @@ int main(int argc, char *argv[]){
     sleep(atoi(argv[1])); 
     //exit 
     return 0; 
+}
+
+
+void *Producer(void *param)
+{
+    // Do work
+}
+
+void *Consumer(void *param)
+{
+    // Do work
 }
