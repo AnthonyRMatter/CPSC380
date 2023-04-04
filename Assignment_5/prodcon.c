f564b812d61a577255670c32cfd1058098694c14
AnthonyMatter <anthonymatter12@gmail.com>
2023-04-03 23:59:47 -0700

    Correcting syntax errors

--- Assignment_5/prodcon.c
+++ Assignment_5/prodcon.c
@@ -3,27 +3,27 @@
 //empty semaphore 
 //full semaphore
 //mutex
-​
+
 // at least 3 threads 
 // main, producer (can have more), consumer (can have more)
 //producer: random data -> data = rand () % 255; 
 //consumer: 
-​
+
 //buffer 
 //set number of num_items yourself 
 //checksum 
-​
+
 #include "buffer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include<unistd.h>
-​
+
 buffer_item buffer[BUFFER_SIZE]; 
-​
+
 int insert_item(buffer_item *item){
     //return 0 if successful, -1 if error
 }
-​
+
 int remove_item(buffer_item *item){
     //return 0 if successful, -1 if error
 }
