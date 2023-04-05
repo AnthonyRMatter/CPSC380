21235358df99dd22601c8e9cfc5a3fe57713f23e
AnthonyMatter <anthonymatter12@gmail.com>
2023-04-05 10:46:33 -0700

    Implemented Internet Checksum

--- Assignment_5/prodcon.c
+++ Assignment_5/prodcon.c
@@ -25,29 +25,56 @@ void *Producer(void *param); /*Producer Thread*/
 void *Consumer(void *param); /*Consumer Thread*/
 int thread;
 
-unsigned int checksum(unsigned int *ptr, int numberbytes)
-{
-    unsigned long total;
-    for(total = 0; numberbytes > 1; numberbytes -= 2)
-    {
-        total += *ptr++;
-    }
-    if(numberbytes == 1)
-    {
-        total += *(unsigned char*)ptr;
-    }
+// unsigned int checksum(unsigned int *ptr, int numberbytes)
+// {
+//     unsigned long total;
+//     for(total = 0; numberbytes > 1; numberbytes -= 2)
+//     {
+//         total += *ptr++;
+//     }
+//     if(numberbytes == 1)
+//     {
+//         total += *(unsigned char*)ptr;
+//     }
     
-    total = (total >> 16) + (total & 0xffff);
-    total += (total >> 16);
-    return (unsigned int)(total);
+//     total = (total >> 16) + (total & 0xffff);
+//     total += (total >> 16);
+//     return (unsigned int)(total);
+// }
+
+uint16_t checksum(char *addr, uint32_t count)
+{
+    register uint32_t sum = 0;
+
+    uint16_t *buf = (uint16_t *)addr;
+
+    while(count > 1)
+    {
+        sum += *(buf)++;
+        count -= 2;
+    }
+
+    // Add left-over byte, if any
+    if(count > 0)
+    {
+        sum += *addr;
+    }
+
+    // Fold 32-bit sum to 16 bits
+    while(sum >> 16)
+    {
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+
+    return (~sum);
 }
 
 
 
 int insert_item(buffer_item *item){
     // Calculate CheckSum
-    int numberbytes = strlen(item->buffer);
-    unsigned int *ptr = (unsigned int *)item->buffer;
+    uint32_t numberbytes = strlen(item->buffer);
+    char *ptr = (uint8_t *)item->buffer;
     item->cksum = checksum(ptr, numberbytes);
     
     //return 0 if successful, -1 if error
@@ -85,8 +112,6 @@ int main(int argc, char *argv[]){
         thread = pthread_create(&consumerID, &consumerAttr, Consumer, NULL);
         consumerThreads--;
     }
-
-
 
     //producer thread calls insert()
     //consumer thread calls remove()
